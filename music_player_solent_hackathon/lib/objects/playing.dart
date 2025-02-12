74e9131fe1b4a2319706dc037f40cfa9b56434f7
SollyBunny <sollybunn@gmail.com>
2025-02-12 11:48:59 +0000

    Chaneg classes

---
+++ music_player_solent_hackathon/lib/objects/playing.dart
@@ -0,0 +1,5 @@
+import 'package:music_player_solent_hackathon/objects/song.dart';
+
+class Playing {
+  List<Song> songs = [];
+}
--- music_player_solent_hackathon/lib/objects/playlist.dart
+++ music_player_solent_hackathon/lib/objects/playlist.dart
@@ -1,14 +1,8 @@
 import 'package:music_player_solent_hackathon/objects/song.dart';
 
 class Playlist {
-  List<Song> songs;
-  String thumbnailUri;
-  String description;
-  String name;
-
-  Playlist(
-      {this.songs = const [],
-      this.thumbnailUri = "",
-      this.description = "",
-      this.name = ""});
+  List<Song> songs = [];
+  String thumbnailUri = "Placeholder Thumbnail";
+  String description = "Placeholder Description";
+  String name = "Placeholder Name";
 }
