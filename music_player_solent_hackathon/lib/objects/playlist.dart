8196ccae82b15b4ed9f9c4460f7a4925c3f54017
SollyBunny <sollybunn@gmail.com>
2025-02-12 12:30:32 +0000

    push

--- music_player_solent_hackathon/lib/objects/playlist.dart
+++ music_player_solent_hackathon/lib/objects/playlist.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:music_player_solent_hackathon/objects/song.dart';
 
 class Playlist {
+  final ValueNotifier<bool> needsUpdate = ValueNotifier(false);
   List<Song> songs = [];
   String thumbnailUri = "Placeholder Thumbnail";
   String description = "Placeholder Description";
