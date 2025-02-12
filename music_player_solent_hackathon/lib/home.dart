2d1c6010d812779b82ca2f98c03450f704adfde5
SollyBunny <sollybunn@gmail.com>
2025-02-12 15:28:09 +0000

    playing

--- music_player_solent_hackathon/lib/home.dart
+++ music_player_solent_hackathon/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:music_player_solent_hackathon/objects/player.dart';
 import 'package:music_player_solent_hackathon/tabs/homepage.dart';
 import 'package:music_player_solent_hackathon/tabs/import.dart';
 import 'package:music_player_solent_hackathon/tabs/playlist.dart';
@@ -40,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    AudioPlayer audioplayer = context.watch<AudioPlayer>();
+    Player player = context.watch<Player>();
     return DefaultTabController(
         length: 5,
         child: Scaffold(
---
+++ music_player_solent_hackathon/lib/objects/player.dart
@@ -0,0 +1,77 @@
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:music_player_solent_hackathon/objects/playlist.dart';
+import 'package:music_player_solent_hackathon/objects/song.dart';
+
+enum RepeatMode {
+  repeatNone,
+  repeatSingle,
+  repeatPlaylist
+}
+
+class Player {
+  AudioPlayer player = AudioPlayer();
+  Playlist playlist = Playlist();
+  bool buffering = false;
+  bool playing = false;
+  Song? song;
+  RepeatMode repeatMode = RepeatMode.repeatNone;
+  bool shuffle = false; // TODO implement shuffle
+  int _ptr = 0;
+  _popSong() {
+    if (playlist.songs.isEmpty) return;
+    if (_ptr < 0) _ptr = 0;
+    if (_ptr >= playlist.songs.length) {
+      if (repeatMode != RepeatMode.repeatPlaylist) return;
+      _ptr = 0;
+    }
+    if (repeatMode == RepeatMode.repeatSingle) {
+      return playlist.songs[_ptr];
+    } else {
+      return playlist.songs.removeAt(_ptr);
+    }
+  }
+  playForce() {
+    song = _popSong();
+    player.play(UrlSource(song!.uri));
+  }
+  play() {
+    if (playing) return;
+    if (_ptr >= playlist.songs.length) _ptr = 0;
+    playForce();
+  }
+  pause() {
+    player.pause();
+  }
+  resume() {
+    player.resume();
+  }
+  // next() {
+  //   player.stop();
+  //   _ptr += 1;
+    
+  // }
+  Player() {
+    player.onPlayerStateChanged.listen((_) {
+      switch (player.state) {
+        case PlayerState.completed:
+          song = null;
+          playForce();
+          break;
+        case PlayerState.stopped:
+          song = null;
+          playing = false;
+          break;
+        case PlayerState.paused:
+          playing = false;
+          break;
+        case PlayerState.playing:
+          playing = true;
+          break;
+        case PlayerState.disposed:
+          throw "Unexpected state";
+      }
+    });
+  }
+}
